bb4ba651d5db034080a340685380f15209506770
Aziz Anwar <azisanw19@gmail.com>
2023-04-30 15:44:31 +0700

    Preview Composable

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 1. Composable function
     - Add Text Element
     - Fungsi composable hanya dapat dipanggil dari fungsi composable lain
-    - 
+    - Anotasi preview digunakan untuk melihat pratinjau 
+    - Anotasi preview hanya dapat digunakan pada fungsi composable yang tidak menggunakan parameter
--- app/src/main/java/com/canwar/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/canwar/jetpackcompose/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,8 +16,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-// fungsi dengan anotasi Composable hanya dapat dipanggil dari fungsi compose lain
+// Fungsi dengan anotasi Composable hanya dapat dipanggil dari fungsi compose lain
 @Composable
 fun MessageCard(name: String) {
     Text(text = "Hello $name!")
+}
+
+// Anotasi preview digunakan untuk melihat pratinjau
+// Anotasi preview hanya dapat digunakan pada fungsi composable yang tidak menggunakan parameter
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(name = "Aziz Anwar")
 }
